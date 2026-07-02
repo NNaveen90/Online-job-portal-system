df5cc241b6f0cc81027e3302a1754ec8c4d616e4
Naveen Nagilla <naveen.nagilla9390@gmail.com>
2026-07-02 10:06:57 +0530

    Externalize DB credentials to properties file

--- src/main/java/com/jobportal/util/DBConnection.java
+++ src/main/java/com/jobportal/util/DBConnection.java
@@ -1,24 +1,28 @@
 package com.jobportal.util;
 
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.util.Properties;
 
-public class DBConnection 
-{
-	public static Connection getConnection()
-	{
-		Connection con = null;
-		
-		try 
-		{
-			Class.forName("com.mysql.cj.jdbc.Driver");
-			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/online_job_portal","root","root");
-		}
-		catch (Exception e) 
-		{
-			e.printStackTrace();
-		}
-		return con;
-		
-	}
+public class DBConnection {
+    public static Connection getConnection() {
+        Connection con = null;
+        try {
+            Properties props = new Properties();
+            InputStream input = DBConnection.class.getClassLoader()
+                    .getResourceAsStream("db.properties");
+            props.load(input);
+
+            String url = props.getProperty("db.url");
+            String user = props.getProperty("db.user");
+            String password = props.getProperty("db.password");
+
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection(url, user, password);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return con;
+    }
 }
